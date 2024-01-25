79382af0c8061095f11bf10fb4c9a5f7afc308f7
SlavaB <ezh.bogov@mail.ru>
2024-01-25 14:40:34 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using System.Linq;
+
+namespace reg
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
